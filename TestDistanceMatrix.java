1be36e16d4dcfa8e1be2e3e588684f3446ea0304
nbt67 <norbert.tomas01@estudiant.upf.edu>
2021-10-23 19:14:39 +0200

    main

--- TestDistanceMatrix.java
+++ TestDistanceMatrix.java
@@ -1,14 +1,14 @@
-import Main.DistanceMatrix;
-import Main.DisplayMatrix;
-import Main.Point;
-
 public class TestDistanceMatrix {
     public static void main (String[] args){
         DistanceMatrix matrix1 = new DistanceMatrix();
         
         matrix1.addCity(10.0, 20.0, "barcelona");
+        matrix1.addCity(50.0, 10.0, "barcelona");
+        matrix1.addCity(40.0, 20.0, "barcelona");
+        matrix1.addCity(80.0, 70.0, "barcelona");
         
+        String st = matrix1.getCityName(0);       
+        System.out.println(st);
 
-    
     }
 }
