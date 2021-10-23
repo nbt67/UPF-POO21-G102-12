94b947091795c08d28c84ee96792c496100951dc
nbt67 <norbert.tomas01@estudiant.upf.edu>
2021-10-23 18:10:32 +0200

    init

--- DistanceMatrix.java
+++ DistanceMatrix.java
@@ -1,17 +1,48 @@
 package Main;
+import Main.DistanceMatrix;
+import Main.Point;
+import java.util.LinkedList;
+
+import distancematrix.Matrix;
 
 public class DistanceMatrix {
-    private Point list[];
-    private double matrix[][];
+    private LinkedList<LinkedList<Double>> rows;
+    private LinkedList<Point> cities;
+    
 
     public void DistanceMatrix (){
+    }
+
+    public void addCity (double x, double y, String name){
+        Point point1 = new Point(x, y, name);
+        cities.add(point1);
+
+        Point first = cities.get(0);
+        String xd = first.getName();
+
+        System.out.println(xd);
+    }
+    
+    public String getCityName (Integer index){
+        return cities.get(index);
+    }
+
+    public Integer geyNoOfCities (){
+        return cities.size();
+    }
+
+    public void createDistanceMatrix (){
+        
+    }
+
+    public Real getDistance (Integer index1, Integer index2){
 
     }
+
 }    
     /*public addPoint(Point p){
         p.getX();
         p.getY();
-
 
     }
 }
---
+++ TestDistanceMatrix.java
@@ -0,0 +1,12 @@
+import Main.DistanceMatrix;
+import Main.DisplayMatrix;
+
+public class TestDistanceMatrix {
+    public static void main (String[] args){
+        DistanceMatrix matrix1 = new DistanceMatrix();
+        
+        matrix1.addCity(10.0, 20.0, "barcelona");
+         
+    
+    }
+}
