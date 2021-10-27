c922b2fb5777f1afa8141cb3445b77fcd982e30b
nbt67 <92040677+nbt67@users.noreply.github.com>
2021-10-27 16:41:16 +0200

    Add files via upload

---
+++ Lab-1/TestDisplayMatrix.java
@@ -0,0 +1,7 @@
+public class TestDisplayMatrix {
+    public static void main (String[] args){
+        DistanceMatrix matrix = new DistanceMatrix();
+        DisplayMatrix display = new DisplayMatrix(matrix);
+        display.setVisible(true);
+    }
+}
---
+++ Lab-1/TestDistanceMatrix.java
@@ -0,0 +1,12 @@
+public class TestDistanceMatrix {
+    public static void main (String[] args){
+        DistanceMatrix matrix1 = new DistanceMatrix();
+        
+        matrix1.addCity(10.0, 20.0, "barcelona");
+        matrix1.addCity(50.0, 10.0, "madrid");
+        matrix1.addCity(40.0, 20.0, "paris");
+        matrix1.addCity(80.0, 70.0, "londres");  
+
+        matrix1.createDistanceMatrix();
+    }
+}
