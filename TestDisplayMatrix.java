3015af24a054400e36dc41c8445fc27834b01878
nbt67 <92040677+nbt67@users.noreply.github.com>
2021-10-27 16:42:23 +0200

    Delete TestDisplayMatrix.java

--- TestDisplayMatrix.java
+++
@@ -1,7 +0,0 @@
-public class TestDisplayMatrix {
-    public static void main (String[] args){
-        DistanceMatrix matrix = new DistanceMatrix();
-        DisplayMatrix display = new DisplayMatrix(matrix);
-        display.setVisible(true);
-    }
-}
