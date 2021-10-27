b8104b770880b90cb20d9530f145fe95b073fa30
nbt67 <norbert.tomas01@estudiant.upf.edu>
2021-10-27 18:00:37 +0200

    main

--- Lab-2/PolygonalRegion.java
+++ Lab-2/PolygonalRegion.java
@@ -6,19 +6,41 @@ public class PolygonalRegion{
     }
 
     public getArea(){
-        double determinant = for i 
-
-        double area = (1/2)
+        double determinant = getDeterminant(); 
+        double area = (1/2)*determinant;
+        return area;
     }
 
     public getDeterminant(){
         int size = Point.size();
+        int accum = 0;
+        int i = 0;
+        int j = 1;
 
-        for (int i = 0; i < size; i++){
-            for (int j = 0; j < size; j++){
-                
-            }
+        while (i != size){
+            x = (Point.get(i)).getX;
+            y = (Point.get(j)).getY;
+            accum += x*y;
+            i++;
+            j++;
         }
+        x = (Point.get(size)).getX;
+        y = (Point.get(0)).getY;
+        accum += x*y;
+        
+        int accum2 = 0;
+        i = 0;
+        j = 1;
+        while (i != size){
+            y = (Point.get(i)).getY;
+            x = (Point.get(j)).getX;
+            accum2 += x*y;
+            i++;
+            j++;
+        }
+        x = (Point.get(0)).getX;
+        y = (Point.get(size)).getY;
+        return accum -accum2;
     }
 
     public draw(Graphics g, double x, double y){
