40b6958654e24dfbb9bd5712d2e50c1384ac5c40
nbt67 <norbert.tomas01@estudiant.upf.edu>
2021-10-29 14:57:44 +0200

    main

--- Lab-2/PolygonalRegion.java
+++ Lab-2/PolygonalRegion.java
@@ -1,49 +1,54 @@
+import java.util.LinkedList;
+
 public class PolygonalRegion{
-    private LinkedList<Point> Point;
+    private LinkedList<Point> point;
 
     public PolygonalRegion (LinkedList<Point> l){
-        Point = new LinkedList<Point>();
+        point = new LinkedList<Point>();
     }
 
-    public getArea(){
+    public double getArea(){
         double determinant = getDeterminant(); 
         double area = (1/2)*determinant;
         return area;
     }
 
-    public getDeterminant(){
-        int size = Point.size();
+    public  double getDeterminant(){
+        int size = point.size();
         int accum = 0;
         int i = 0;
         int j = 1;
+        double x = 0;
+        double y = 0;
+
 
         while (i != size){
-            x = (Point.get(i)).getX;
-            y = (Point.get(j)).getY;
+            x = (point.get(i)).getX();
+            y = (point.get(j)).getY();
             accum += x*y;
             i++;
             j++;
         }
-        x = (Point.get(size)).getX;
-        y = (Point.get(0)).getY;
+        x = (point.get(size)).getX();
+        y = (point.get(0)).getY();
         accum += x*y;
         
         int accum2 = 0;
         i = 0;
         j = 1;
         while (i != size){
-            y = (Point.get(i)).getY;
-            x = (Point.get(j)).getX;
+            y = (point.get(i)).getY();
+            x = (point.get(j)).getX();
             accum2 += x*y;
             i++;
             j++;
         }
-        x = (Point.get(0)).getX;
-        y = (Point.get(size)).getY;
+        x = (point.get(0)).getX();
+        y = (point.get(size)).getY();
         return accum -accum2;
     }
 
-    public draw(Graphics g, double x, double y){
+    public void draw(Graphics g, double x, double y){
         
     }
 }
