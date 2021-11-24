0d9d98362f45a06bb26d025201c7472762571a3b
nbt67 <norbert.tomas01@estudiant.upf.edu>
2021-11-24 16:54:28 +0100

    Update MyMap.java

--- Lab-3/MyMap.java
+++ Lab-3/MyMap.java
@@ -27,12 +27,12 @@ public class MyMap extends javax.swing.JPanel {
 
         //Cities
         List< City > cities1 = new ArrayList<>();
-        cities1.add(new City (800.0, 600.0, "Girona", 100));
-        City capital1 = new City (800.0, 550.0, "BCN", 500);
+        cities1.add(new City (800.0, 600.0, "Paris", 100));
+        City capital1 = new City (800.0, 550.0, "Lyon", 500);
 
         List< City > cities2 = new ArrayList<>();
-        cities2.add(new City (200.0, 900.0, "Zaragoza", 150));
-        City capital2 = new City (350.0, 900.0, "Madrid", 200);
+        cities2.add(new City (200.0, 900.0, "BCN", 150));
+        City capital2 = new City (350.0, 900.0, "Girona", 200);
 
         //Countries (PolygonalRegions)
         List< Country > polygonalRegions1 = new ArrayList<>();
