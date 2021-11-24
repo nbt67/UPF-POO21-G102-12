c4d7d0be53499ba45263ec2e9e30ff04f640f2c2
nbt67 <92040677+nbt67@users.noreply.github.com>
2021-11-24 16:57:57 +0100

    Add files via upload

---
+++ GeoPoint.java
@@ -0,0 +1,20 @@
+import java.awt.Graphics;
+
+public class GeoPoint extends Point{
+    private String name;
+
+    public GeoPoint(double xi, double yi, String n){
+        super(xi, yi, n);
+        name = n;
+    }    
+
+    public void draw(java.awt.Graphics g, double x, double y){
+        int xInt = (int) Math.round(super.getX());        
+        int yInt = (int) Math.round(super.getY()); 
+        int width = (int) Math.round(x);        
+        int height = (int) Math.round(y);  
+
+        g.drawOval(xInt, yInt, width, height);
+        g.drawString(name, xInt, yInt);
+    }
+}
